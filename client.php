735a8f8ea55690151ac99388eb254468727ad888
Hnin Yee Nwe <hninyeenwe2@gmail.com>
2020-11-18 10:14:46 +0630

    adding client page

---
+++ client.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>HSP Digital Marketing Agency</title>
+	<link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="images/16x16 px - png-logo.png">
+</head>
+<body>
+<!-- navbar section  start-->
+	<nav class="navbar sticky-top navbar-expand-lg navbar-light navbar-fixed bg-dark" id="navbar">
+		<a class="navbar-brand ml-5" href="index.php"><img src="images/87x87 - png - logo.png"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse ml-5" id="navigation">
+			<ul class="navbar-nav nav-pills">
+				<li class="nav-item pr-4 mb-2">
+					<a href="index.php" class="nav-link"><i class="fas fa-home mr-2"></i>ပင်မစာမျက်နှာ</a>
+				</li>
+
+		      <!-- <li class="nav-item pr-4 mb-2">
+		      		 <div class="dropdown">
+					<a href="#" class="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					သုံးစွဲသူများ</a>
+					<div class="dropdown-menu" dropdownMenu>
+						<a href="#" class="dropdown-item">aclkjl</a>
+						<a href="#" class="dropdown-item">actjk </a>
+						<a href="#" class="dropdown-item">action</a>
+					</div>
+				</div>  -->
+
+				<li class="nav-item pr-4 mb-2 dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-friends mr-2"></i>သုံးစွဲသူများ</a>
+				 	<div class="dropdown-menu p-0" aria-labelledby="navbarDropdown">
+				  		<a class="dropdown-item" href="coffeekh.php">Coffee KH</a>
+	          			<a class="dropdown-item" href="herbal.php">Herbal Paradise</a>
+	          			<a class="dropdown-item" href="mingalarBarPyay.php">Mingalarbar Pyay</a>
+	          			<a class="dropdown-item" href="power_mobile.php">Power Mobile</a>
+	        		</div>
+        		</li>
+
+		        <!-- <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" href="#client" id="dropdownMenu" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-friends mr-2"></i>သုံးစွဲသူများ  
+		        	<div class="dropdown-menu" dropdownMenu>
+					    <a class="dropdown-item" href="#">Action</a>
+					    <a class="dropdown-item" href="#">Another action</a>
+					    <a class="dropdown-item" href="#">Something else here</a>
+					</div>
+		        </a> -->
+		      <!-- </li> -->
+
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link" href="aboutus.php"><i class="fas fa-user mr-2"></i>ကျွန်ုပ်တို့အကြောင်း</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link" href="service.php"><i class="fas fa-cog mr-2"></i>ဝန်ဆောင်မှုများ</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link" href="#contact"><i class="fas fa-phone mr-2"></i>ဆက်သွယ်ရန်</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link" href="blog.php"><i class="fas fa-th mr-2"></i>ဘလော့ခ်</a>
+		      </li>
+		    </ul>
+		</div>
+	</nav>
+<!-- navbar section end -->
+
+<!-- client section start -->
+	<div class="container mt-5 p-3 mb-5 rounded" id="clients">
+		<h4 class="borderimg text-center">သုံးစွဲသူများ</h4>
+	</div>
+	<div class="container-fluid" id="client1">
+		<div class="row">
+			<div class="col-md-6 col-lg-4 my-3">
+				<div class="card m-auto" style="width: 18rem;">
+					<a href="coffeekh.php">
+						<img src="images/Post-1-International Coffee Day.jpg" class="card-img-top" alt="...">
+					</a>
+					  <div class="card-body">
+					    <p class="card-text text-center">Coffee kh</p>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-6 col-lg-4 my-3">
+				<div class="card m-auto" style="width: 18rem;">
+					<a href="herbal.php">
+						<img src="images/HP (2).jpg" class="card-img-top" alt="...">
+					</a>
+					<div class="card-body">
+						<p class="card-text text-center">Herbal Paradis</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-lg-4 my-3">
+				<div class="card m-auto" style="width: 18rem;">
+					<a href="mingalarBarPyay.php">
+					  	<img src="images/cover.png" class="card-img-top" alt="...">
+					</a>
+					  <div class="card-body">
+					    <p class="card-text text-center">Mingalarbar Pyay</p>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-6 col-lg-4 my-3">
+				<div class="card m-auto" style="width: 18rem;">
+					<a href="power_mobile.php">
+					  	<img src="images/Post 1 - Welcome Post.jpg" class="card-img-top" alt="...">
+					</a>
+					  <div class="card-body">
+					    <p class="card-text text-center">Power Mobile</p>
+					  </div>
+				</div>
+			</div>
+		</div>
+	</div>	
+<!-- client section end -->
+
+<!-- contact section start-->
+	<div id="contact">
+		<div class="container-fluid mb-2">
+			<div class="row">
+				<div class="col-lg-4">
+					<h4 class="text-center mt-4">တည်နေရာ:</h4><br>
+					<iframe  class="ml-5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.6758215261694!2d95.21554196784714!3d18.819093632987403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c7835073d5366d%3A0x85271bdcd32f269a!2sBogyoke%20Road%2C%20Pye!5e1!3m2!1sen!2smm!4v1603184661535!5m2!1sen!2smm" width="300" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+				</div>
+				<div class="col-lg-4 mt-3 my-2">
+					<h4 class="text-center mt-3">ဆက်သွယ်ရန်အချက်အလက်:</h4>
+					<p class="font-rb font-size-16 mt-4 ml-4"><i class="fas fa-phone-alt mr-2"></i>09 893 199994</p>
+
+					<p class="font-rb font-size-16 ml-4"><a href="hsp.marketingagency@gmail.com" class="text-dark"><i class="far fa-envelope mr-2 "></i>hsp.marketingagency@gmail.com</a></p>
+
+					<p class="font-rb font-size-16 ml-4"><a href="https://web.facebook.com/hspdigitalmarketingagency" class="text-dark"><i class="fab fa-facebook-square mr-2"></i>HSP Digital Marketing Agency</a></p>
+
+					<p class="font-rb font-size-16 ml-4"><i class="fas fa-home mr-2"></i>No. 157,Bogyoke Road,Ywar Bae` East Ward,Near CB Bank (2),<br>Pyay,Bago Region,Myanmar,18156</p>
+				</div>
+				<div class="col-lg-4 mt-3 my-2">
+					<h4 class="text-center mt-3">ဆက်သွယ်ရန်ပုံစံ:</h4>
+					<form class="mt-4">
+						<input type="text" class="form-control " placeholder="အမည်">
+						<input type="email" class="form-control mt-3" placeholder="အီးမေးလ်">
+						<textarea cols="30" rows="3" class="form-control my-3" placeholder="မက်ဆေ့ချ်"></textarea>
+						<button class="btn btn-danger btn-block" type="submit">ဆက်သွယ်ပါ</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		<footer class="text-muted mt-5 text-center">
+				copyright &copy;2020. HSP Digital Marketing Agency Co., ltd. All right reserved.		
+		</footer>
+	</div>
+<!-- contact section end -->
+
+	<script src="library/bootstrap/jquery-3.4.1.slim.min.js"></script>
+	<script src="library/bootstrap/popper.min.js"></script>
+	<script src="library/bootstrap/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
+</body>
+</html>
---
+++ power_mobile.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>HSP Digital Marketing Agency</title>
+	<link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+	<link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shorcut icon" href="images/48x48 px - png-logo.png">
+</head>
+<body>
+
+<!-- navbar section  start-->
+	<nav class="navbar sticky-top navbar-expand-lg navbar-light navbar-fixed bg-dark" id="navbar">
+		<a class="navbar-brand ml-5" href="index.php"><img src="images/87x87 - png - logo.png"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse ml-5" id="navigation">
+			<ul class="navbar-nav nav-pills">
+				<li class="nav-item pr-4 mb-2">
+					<a href="index.php" class="nav-link"><i class="fas fa-home mr-2"></i>ပင်မစာမျက်နှာ</a>
+				</li>
+
+		      <!-- <li class="nav-item pr-4 mb-2">
+		      		 <div class="dropdown">
+					<a href="#" class="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					သုံးစွဲသူများ</a>
+					<div class="dropdown-menu" dropdownMenu>
+						<a href="#" class="dropdown-item">aclkjl</a>
+						<a href="#" class="dropdown-item">actjk </a>
+						<a href="#" class="dropdown-item">action</a>
+					</div>
+				</div>  -->
+
+				<li class="nav-item pr-4 mb-2 dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-friends mr-2"></i>သုံးစွဲသူများ</a>
+				 	<div class="dropdown-menu p-0" aria-labelledby="navbarDropdown">
+				  		<a class="dropdown-item" href="coffeekh.php">Coffee KH</a>
+	          			<a class="dropdown-item" href="herbal.php">Herbal Paradise</a>
+	          			<a class="dropdown-item" href="mingalarBarPyay.php">Mingalarbar Pyay</a>
+	          			<a class="dropdown-item" href="power_mobile.php">Power Mobile</a>
+	        		</div>
+        		</li>
+
+		        <!-- <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" href="#client" id="dropdownMenu" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-friends mr-2"></i>သုံးစွဲသူများ  
+		        	<div class="dropdown-menu" dropdownMenu>
+					    <a class="dropdown-item" href="#">Action</a>
+					    <a class="dropdown-item" href="#">Another action</a>
+					    <a class="dropdown-item" href="#">Something else here</a>
+					</div>
+		        </a> -->
+		      <!-- </li> -->
+
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link" href="aboutus.php"><i class="fas fa-user mr-2"></i>ကျွန်ုပ်တို့အကြောင်း</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link" href="service.php"><i class="fas fa-cog mr-2"></i>ဝန်ဆောင်မှုများ</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link" href="#contact"><i class="fas fa-phone mr-2"></i>ဆက်သွယ်ရန်</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link" href="blog.php"><i class="fas fa-th mr-2"></i>ဘလော့ခ်</a>
+		      </li>
+		    </ul>
+		</div>
+	</nav>
+<!-- navbar section end -->
+	
+<!-- power mobile section start -->
+	<section id="power-mobile">
+		<div class="container">
+			<div class="row my-5">
+				<div class="col-md-8">
+					<div class="container p-3 my-5 mt-5 borderimg rounded" id="power">
+						<h3 class="text-center font-rb">Power Mobile</h3>
+					</div>
+				</div>
+				<div class="col-md-4">
+		  			<video width="350" height="200" class="ml-4" controls>
+						<source src="images/Post-3-Chatbot အသုံးပြုပုံ.mp4" type="video/mp4">
+					</video>
+				</div>
+			</div>
+			
+			<div class="row mt-3 mb-5">
+				<div class="col-lg-3 col-md-3 my3">
+					<img src="images/Post 1 - Welcome Post.jpg" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3 my3">
+					<img src="images/Post 2 - Auto Reply Chatbot.jpg" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3 my3">
+					<img src="images/Post-5-Request for messenger.jpg" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3 my3">
+					<img src="images/Post-4-Knowledge Battery.jpg" class="img-fluid" alt="">
+				</div>
+			</div>
+		</div>
+	</section>
+<!-- power mobile section end -->
+
+<!-- contact section start-->
+	<div id="contact">
+		<div class="container-fluid mb-2">
+			<div class="row">
+				<div class="col-lg-4">
+					<h4 class="text-center mt-4">တည်နေရာ:</h4><br>
+					<iframe  class="ml-5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.6758215261694!2d95.21554196784714!3d18.819093632987403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c7835073d5366d%3A0x85271bdcd32f269a!2sBogyoke%20Road%2C%20Pye!5e1!3m2!1sen!2smm!4v1603184661535!5m2!1sen!2smm" width="300" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+				</div>
+				<div class="col-lg-4 mt-3 my-2">
+					<h4 class="text-center mt-3">ဆက်သွယ်ရန်အချက်အလက်:</h4>
+					<p class="font-rb font-size-16 mt-4 ml-4"><i class="fas fa-phone-alt mr-2"></i>09 893 199994</p>
+
+					<p class="font-rb font-size-16 ml-4"><a href="hsp.marketingagency@gmail.com" class="text-dark"><i class="far fa-envelope mr-2 "></i>hsp.marketingagency@gmail.com</a></p>
+
+					<p class="font-rb font-size-16 ml-4"><a href="https://web.facebook.com/hspdigitalmarketingagency" class="text-dark"><i class="fab fa-facebook-square mr-2"></i>HSP Digital Marketing Agency</a></p>
+
+					<p class="font-rb font-size-16 ml-4"><i class="fas fa-home mr-2"></i>No. 157,Bogyoke Road,Ywar Bae` East Ward,Near CB Bank (2),<br>Pyay,Bago Region,Myanmar,18156</p>
+				</div>
+				<div class="col-lg-4 mt-3 my-2">
+					<h4 class="text-center mt-3">ဆက်သွယ်ရန်ပုံစံ:</h4>
+					<form class="mt-4">
+						<input type="text" class="form-control " placeholder="အမည်">
+						<input type="email" class="form-control mt-3" placeholder="အီးမေးလ်">
+						<textarea cols="30" rows="3" class="form-control my-3" placeholder="မက်ဆေ့ချ်"></textarea>
+						<button class="btn btn-danger btn-block" type="submit">ဆက်သွယ်ပါ</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		<footer class="text-muted mt-5 text-center">
+				copyright &copy;2020. HSP Digital Marketing Agency Co., ltd. All right reserved.		
+		</footer>
+	</div>
+<!-- contact section end -->
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/ulg/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>		
+</body>
+</html>
